c6542e68c607e2e3cae370cbdfea50a28b13a77b
kaypaint <kelanmpainter@gmail.com>
2022-10-23 22:39:16 -0500

    Exit 10/23

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,15 +4,15 @@ const Home = () => {
     return (
         <div>
             <div>
-                <h3>Welcome to...</h3>
-                <h1>Stranger's Things!</h1>
+                <h3 id="welcome-one">Welcome to...</h3>
+                <h1 id="welcome-two">Stranger's Things!</h1>
             </div>
-            <ul>
-                <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
-                <Link to="/posts">Posts</Link>
-            </ul>
+            <div id="nav">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/login">Login</Link>
+                <Link className="nav-link" to="/register">Register</Link>
+                <Link className="nav-link" to="/posts">Posts</Link>
+            </div>
         </div>
     )
 }
