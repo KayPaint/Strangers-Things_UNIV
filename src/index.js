529897b33a677a78c66a79024fe0032108e76a2b
kaypaint <kelanmpainter@gmail.com>
2022-10-23 22:04:05 -0500

    Added Home, Login, Posts, and Register as empty components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from "./components/Home.js";
+import Login from "./components/Login.js";
+import Register from "./components/Register.js";
+import Posts from "./components/Posts.js";
 
 const App = () => {
-  return <div>
-    Hello World
-  </div>
+  return (<BrowserRouter>
+    <Route path="/" component={Home} />
+    <Route path="/login" component={Login} />
+    <Route path="/register" component={Register} />
+    <Route path="/posts" component={Posts} />
+  </BrowserRouter>
+  )
 }
 
 ReactDOM.render(
