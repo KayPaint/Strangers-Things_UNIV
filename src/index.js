9749b34c82dfcac7788b7c4319a498815efb33cb
kaypaint <kelanmpainter@gmail.com>
2022-10-23 22:24:31 -0500

    10/23 Exit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+
 import Home from "./components/Home.js";
 import Login from "./components/Login.js";
 import Register from "./components/Register.js";
