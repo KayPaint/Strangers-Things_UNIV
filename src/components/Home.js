22f5c21c2c915da2d03da551ef59b24d4c5b7712
kaypaint <kelanmpainter@gmail.com>
2022-10-23 22:25:20 -0500

    Route links have been added and work, also included welcome message

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom"
+
 const Home = () => {
     return (
         <div>
-
+            <div>
+                <h3>Welcome to...</h3>
+                <h1>Stranger's Things!</h1>
+            </div>
+            <ul>
+                <Link to="/">Home</Link>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+                <Link to="/posts">Posts</Link>
+            </ul>
         </div>
     )
 }
