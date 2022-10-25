a295d9a20425b20916aab69d30607ed7ac3a0639
kaypaint <kelanmpainter@gmail.com>
2022-10-25 13:18:23 -0500

    Added two new components, fixed header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <div className='footer-fixed'>
+            <div className='footer-container'>
+                <h3>FOOTER H3</h3>
+                <h1>FOOTER H1</h1>
+            </div> 
+        </div>
+    )
+}
+
+export { Footer }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div className='header-fixed'>
+            <div className='header-container'>
+                <h3>Welcome to...</h3>
+                <h1>Stranger's Things</h1>
+            </div> 
+        </div>
+    )
+}
+
+export { Header }
