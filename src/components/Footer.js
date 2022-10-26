2bc929adfee74cb0decb7d97e217558043d26645
kaypaint <kelanmpainter@gmail.com>
2022-10-25 19:51:38 -0500

    Added footer with nav

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
+import { Nav } from "./Nav.js"
+
 const Footer = () => {
     return (
         <div className='footer-fixed'>
             <div className='footer-container'>
-                <h3>FOOTER H3</h3>
-                <h1>FOOTER H1</h1>
+                <Nav />
             </div> 
         </div>
     )
