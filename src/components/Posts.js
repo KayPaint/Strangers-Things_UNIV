c9803c6306a810118fcd0676c412b875ba7da6dc
kaypaint <kelanmpainter@gmail.com>
2022-10-25 19:48:11 -0500

    Posts component complete

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,40 @@
-const Posts = () => {
-    return(
-        <div>
-            <h1>Posts is rendering</h1>
+import { useEffect } from "react";
+import { fetchPosts } from "../api"
+import { SinglePost } from "./SinglePost.js"
+
+const Posts = (props) => {
+
+    const { posts, setPosts } = props
+
+    useEffect(() => {
+        const getPosts = async () => {
+            try {
+                let response = await fetchPosts();
+                console.log("Response", response)
+                setPosts(response)
+                return response;    
+            } catch {
+                console.error()
+            }
+        } 
+        getPosts()
+    }, []);
+
+    return (<>
+        <div className="posts-container">
+            {posts.map((item) => {
+                return <SinglePost 
+                    key={item._id} 
+                    title={item.title}
+                    description={item.description}
+                    location={item.location}
+                    price={item.price}
+                    willDeliver={item.willDeliver}
+                    />
+            })}
         </div>
-    )
+    </>);
+
 }
 
 export default Posts
