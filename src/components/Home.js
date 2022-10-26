e81aa5e50fc147e76512d44ba85909d5bfb172ac
kaypaint <kelanmpainter@gmail.com>
2022-10-25 19:52:13 -0500

    Updated Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,11 @@
-import { Link } from "react-router-dom"
+import { Header } from "./Header.js"
+import { Footer } from "./Footer.js"
 
 const Home = () => {
     return (
         <div>
-            <div>
-                <h3 id="welcome-one">Welcome to...</h3>
-                <h1 id="welcome-two">Stranger's Things!</h1>
-            </div>
-            <div id="nav">
-                <Link className="nav-link" to="/">Home</Link>
-                <Link className="nav-link" to="/login">Login</Link>
-                <Link className="nav-link" to="/register">Register</Link>
-                <Link className="nav-link" to="/posts">Posts</Link>
-            </div>
+            <Header />
+            <Footer />
         </div>
     )
 }
