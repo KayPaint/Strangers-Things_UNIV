07f6a2907d011bcc40be76e83a9f914f1dec2cda
kaypaint <kelanmpainter@gmail.com>
2022-10-27 23:28:21 -0500

    Exitsave 10/27

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useEffect } from "react";
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch, Link, useHistory } from 'react-router-dom'
-import { fetchPosts } from "./api/index.js"
+import { fetchPosts, fetchGuest } from "./api/index.js"
 
 import Home from "./components/Home.js";
 import AccountForm from "./components/AccountForm.js";
@@ -12,25 +12,45 @@ const App = () => {
 
   const [posts, setPosts] = useState([]);
   const [token, setToken] = useState(window.localStorage.getItem("token")||"");
+  const [guest, setGuest] = useState(null);
   const history = useHistory();
 
   const logOut = () => {
     setToken("");
+    setGuest(null);
     history.push("/");
   }
 
   useEffect(() => {
     const getPosts = async () => {
-        try {
-            let response = await fetchPosts();
-            console.log("Response", response)
-            setPosts(response)    
-        } catch {
-            console.error()
-        }
+        // try {
+        //     let response = await fetchPosts();
+        //     console.log("Response", response)
+        //     setPosts(response)    
+        // } catch {
+        //     console.error()
+        // }
+      const {error, posts} = await fetchPosts();
+
+      if (error) {
+        console.error(error)
+      }
+
+      setPosts(result);
     } 
     getPosts()
   }, []);
+
+  useEffect(() => {
+    if (token) {
+      const getGuests = async () => {
+        const {guest} = await fetchGuest(token);
+        console.log("Username", guest);
+        setGuest(guest.username)
+      };
+      getGuests()
+    }
+  }, [token]);
 
   useEffect(() => {
     window.localStorage.setItem("token", token)
@@ -55,7 +75,7 @@ const App = () => {
       </div>
       <Switch>
         <Route exact path="/">
-          <Home />
+          <Home guest={guest}/>
         </Route>
         <Route path="/account/:action">
           <AccountForm setToken={setToken} />
