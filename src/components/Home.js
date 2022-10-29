cba0a3f453683153bb54b15ddb82472c3dde51de
kaypaint <kelanmpainter@gmail.com>
2022-10-28 21:43:38 -0500

    Updated current user status

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = ({guest}) => {
         <>
             <div>
                 <Header />
-                {guest && <h3>Currently logged in as: {guest}</h3>}
+                {guest ? <h3 id="current-user">You are currently logged in as: {guest}</h3> : <h3 id="current-user">Welcome to Stranger's Things</h3>}
                 <Footer />
             </div>
         </>
