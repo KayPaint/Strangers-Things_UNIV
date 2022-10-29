70022da4c180f6ce607aab122c1fdf951f4eb0e0
kaypaint <kelanmpainter@gmail.com>
2022-10-28 19:29:35 -0500

    added create post request

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,62 @@
 export const BASE_URL = "https://strangers-things.herokuapp.com/api";
 export const KEY = "2207-FTB-ET-WEB-PT";
 
+// API HELPER FUNCTIONS
+
+const headerAPI = (token) => {
+    const headers = {
+        "Content-Type": "application/json"
+    }
+
+    if (token) {
+        headers["Authorization"] = `Bearer ${token}`
+    }}
+
+const callAPI = async (endpoint, defaultOptions={}) => {
+    const options = {
+        headers: headerAPI(defaultOptions.token),
+    };
+
+    if (defaultOptions.method) {
+        options.method = defaultOptions.method;
+    }
+
+    if (defaultOptions.body) {
+        options.body = JSON.stringify(defaultOptions.body);
+    }
+    
+    const response = await fetch(`${BASE_URL}/${endpoint}`, options);
+    const result = await response.json();
+
+    return result;
+}
+
+
+// GET REQUESTS
+
+// export const fetchPosts = async () => {
+//     try {
+//         const {success, error, data} = await callAPI('/posts');
+
+//         if (success) {
+//             return {
+//                 error: null,
+//                 posts: data.posts   
+//             }
+//         } else {
+//             return {
+//             error: error.message,
+//             posts: []
+//         }}
+//     } catch (error) {
+//         console.error("An error occured while attempting to fetch posts.", error);
+//         return {
+//             error: "Fetching posts has failed",
+//             posts: []
+//         }
+//     }
+// }
+
 export const fetchPosts = async () => {
     try {
         const response = await fetch(`${BASE_URL}/${KEY}/posts`);
@@ -27,13 +83,50 @@ export const fetchGuest = async (token) => {
     }
 }
 
+
+// POST REQUESTS
+
+// export const registerUser = async(username, password) => {
+//     try {
+//         const {success, error, data} = await callAPI('/users/register', {
+//             method: 'POST',
+//             body: {
+//                 user: {
+//                     username,
+//                     password
+//                 }
+//             }
+//         });
+    
+//         if (success) {
+//             return {
+//                 error: null,
+//                 token: data.token,
+//                 message: data.message
+//             }
+//         } else {
+//             return {
+//                 error: error.message,
+//                 token: null,
+//                 message: null
+//             }
+//         } 
+//     } catch (error) {
+//         console.error("An error occured while attempting to register user", error)
+
+//         return {
+//             error: "Registration has failed",
+//             token: null,
+//             message: null
+//         }
+//     }
+// }
+
 export const registerUser = async(username, password) => {
     try {
         const response = await fetch(`${BASE_URL}/${KEY}/users/register`, {
             method: "POST",
-            headers: {
-              'Content-Type': 'application/json'
-            },
+            headers: headerAPI(),
             body: JSON.stringify({
               user: {
                 username,
@@ -68,5 +161,31 @@ export const loginUser = async(username, password) => {
         return data;
     } catch (error) {
         console.error("An error occured while attempting to login user")
+    }
+}
+
+export const createPost = async (token, title, description, location, price, willDeliver) => {
+    try {
+        const response = await fetch(`${BASE_URL}/${KEY}/posts`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                post: {
+                    title: title,
+                    description: description,
+                    location: location,
+                    price: price,
+                    willDeliver: willDeliver
+                }
+            })
+        });
+        const data = await response.json()
+        console.log("createPost data:", data)
+        return data;
+    } catch (error) {
+        console.error("An error occured while attempting to create post")
     }
 }
